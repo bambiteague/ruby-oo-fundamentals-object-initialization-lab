b108653ccbfe482c725ed3274638111783a5f759
bambi teague <teeegsa@gmail.com>
2020-12-15 12:00:35 -0600

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,32 @@
 
-class Dog
+# class Dog
+#     def initialize(name, breed = "Mutt")
+#         @name = name
+#         @breed = breed
+        
+#     end
+    
+#     def name
+#         @name
+#     end
+
+#     def breed
+#        @breed 
+#     end
+
+# end
+
+class Dog 
     def initialize(name, breed = "Mutt")
         @name = name
         @breed = breed
-        
     end
-    
+
     def name
         @name
     end
 
     def breed
-       @breed 
+        @breed
     end
-
 end
